a42a11b5a3788369b3403f746b46f3536dd755cf
PlaymakerHD <bolapltd@gmail.com>
2025-12-05 09:48:29 +0100

    modified:   lib/utils/number_input_formatter.dart

--- lib/utils/number_input_formatter.dart
+++ lib/utils/number_input_formatter.dart
@@ -3,20 +3,25 @@ import 'package:intl/intl.dart';
 
 /// Formats numbers with commas as user types
 class ThousandsSeparatorInputFormatter extends TextInputFormatter {
-  static final _formatter = NumberFormat('#,###', 'en_US');
+  static final _formatter = NumberFormat('#,###.##', 'en_US');
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue o, TextEditingValue n) {
     if (n.text.isEmpty) return n;
-    final digits = n.text.replaceAll(RegExp(r'[^\d]'), '');
-    if (digits.isEmpty) return o;
-    final num = int.tryParse(digits);
+    final cleaned = n.text.replaceAll(RegExp(r'[^\d.]'), '');
+    if (cleaned.isEmpty) return o;
+    if (cleaned == '.')
+      return TextEditingValue(
+          text: '0.', selection: TextSelection.collapsed(offset: 2));
+    final parts = cleaned.split('.');
+    if (parts.length > 2) return o;
+    final num = double.tryParse(cleaned);
     if (num == null) return o;
-    final formatted = _formatter.format(num);
+    String formatted = _formatter.format(num);
+    if (cleaned.endsWith('.') && !formatted.contains('.')) formatted += '.';
     return TextEditingValue(
-      text: formatted,
-      selection: TextSelection.collapsed(offset: formatted.length),
-    );
+        text: formatted,
+        selection: TextSelection.collapsed(offset: formatted.length));
   }
 }
 
